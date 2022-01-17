6d8e710ff7318f006f17d143cdc266c65edb425f
Marius Thorre <marius.thorre@etu.univ-amu.fr>
2022-01-17 18:30:33 +0100

    refactoring

--- Sorting.py
+++ Sorting.py
@@ -3,12 +3,12 @@ def bullet_tris(lists):
     for i in range(n, 1, -1):
         for j in range(0, n-1):
             if lists[j] > lists[j + 1]:
-                permute(list, j, j + 1)
+                swap(list, j, j + 1)
 
     return lists
 
 
-def permute(list, j, j_plus_un):
+def swap(list, j, j_plus_un):
     tmp = list[j_plus_un]
     list[j_plus_un] = list[j]
     list[j] = tmp
