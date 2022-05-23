93745c3d6f5030f688303518740dfe3daaf25897
Morpheus5828 <marius.thorre@etu.univ-amu.fr>
2022-05-23 13:12:42 +0200

    Add insertion sorting

--- Sorting.py
+++ Sorting.py
@@ -1,15 +1,30 @@
 def bullet_tris(lists):
     n = len(lists)
     for i in range(n, 1, -1):
-        for j in range(0, n-1):
+        for j in range(0, n - 1):
             if lists[j] > lists[j + 1]:
-                swap(list, j, j + 1)
+                swap(lists, j, j + 1)
 
     return lists
 
 
-def swap(list, j, j_plus_un):
-    tmp = list[j_plus_un]
-    list[j_plus_un] = list[j]
-    list[j] = tmp
+def insertion(lists):
+    n = len(lists)
+    for i in range(1, n):
+        key = lists[i]
+        j = i - 1
+        while (j >= 0) and (lists[j] > key):
+            lists[j + 1] = lists[j]
+            j -= 1
+        lists[j + 1] = key
+    return lists
 
+
+def swap(lists, j, j_plus_un):
+    tmp = lists[j_plus_un]
+    lists[j_plus_un] = lists[j]
+    lists[j] = tmp
+
+
+numbers = [13, -2, -5, 6, 8, 0]
+print(insertion(numbers))
